82987f50f8c0f3d96f21ef70767c85a26e4df5d5
Vitor Andrey Lopes Santos <vitor.andreylopess@gmail.com>
2023-04-22 01:31:31 +0000

    adiciona a pasta pages

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,3 @@
 export default function Home () {
-    return (
-        <>
-            <h1>Essa messagem deve mudar vidas</h1>
-        </>
-    )
+    return <h1>Essa mensagem vai mudar o mundo</h1>
 }
