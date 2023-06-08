1e1f85d58b1c15983f1435f0f2f6d9eaf698de51
Vitor Andrey Lopes Santos <vitor.andreylopess@gmail.com>
2023-06-08 13:45:44 +0000

    num ta natural

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,8 @@
-export default function Home () {
-    return <h1>Essa mensagem vai mudar o mundo</h1>
+export default function Home() {
+  return (
+    <div>
+      <h1>Num tá natural...</h1>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwQkbrUtvScI_wwRK1crOqxEEEOG5BZ7MLqw&usqp=CAU" />
+    </div>
+  );
 }
