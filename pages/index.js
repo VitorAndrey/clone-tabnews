6fc2f7cf75bcc15164311ee159d23e0a3ad0a7db
Vitor Andrey Lopes Santos <vitor.andreylopess@gmail.com>
2023-07-02 19:42:46 +0000

    adicionei o editorconfig

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,3 @@
 export default function Home() {
-  return (
-    <div>
-      <h1>Num tá natural...</h1>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRwQkbrUtvScI_wwRK1crOqxEEEOG5BZ7MLqw&usqp=CAU" />
-    </div>
-  );
+  return <h1>Oi</h1>;
 }
