94a0b9e8c20e4ef126e479ed0558ede86b7cf60f
Vitor Andrey Lopes Santos <vitor.andreylopess@gmail.com>
2023-09-27 10:48:13 +0000

    feat: building page

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,27 @@
+import Head from "next/head";
+
 export default function Home() {
-  return <h1>Oi</h1>;
+  return (
+    <>
+      <Head>
+        <title>Tabnews</title>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <link rel="shortcut icon" href="/favicon.svg" />
+      </Head>
+      <main
+        style={{
+          width: "100%",
+          height: "100vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <h1 style={{ fontSize: "4rem" }}>Clone do Tabnews</h1>
+        <p style={{ color: "#aaaaaa" }}>Em construção...</p>
+      </main>
+    </>
+  );
 }
