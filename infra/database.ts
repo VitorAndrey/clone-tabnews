9e987ed074554a74e66d0a98a387e2bda67d3c14
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-08-20 13:13:54 -0300

    fix: remove rows return in db.ts

--- infra/database.ts
+++ infra/database.ts
@@ -11,16 +11,12 @@ async function query(queryObject: { text: string; values: Array<String> }) {
 
   try {
     await client.connect();
-  } catch (error) {
-    console.error("Database connection error");
-  }
 
-  try {
     const res = await client.query(queryObject);
     return res;
   } catch (err) {
     console.error(err);
-    return { rows: [] };
+    throw err;
   } finally {
     await client.end();
   }
