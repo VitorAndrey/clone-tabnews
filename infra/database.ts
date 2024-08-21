e8599c1c4b20f274692ae2282b8a59234cf6bdc4
Vitor Andrey <vitor.andreylopes@gmail.com>
2024-08-21 10:39:17 -0300

    chore: add POSTGRES_CA env var to ssl

--- infra/database.ts
+++ infra/database.ts
@@ -7,7 +7,7 @@ async function query(queryObject: { text: string; values: Array<String> }) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
-    ssl: process.env.NODE_ENV === "development" ? false : true,
+    ssl: getSSLValues(),
   });
 
   try {
@@ -26,3 +26,12 @@ async function query(queryObject: { text: string; values: Array<String> }) {
 export default {
   query: query,
 };
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA)
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+
+  return process.env.NODE_ENV === "development" ? false : true;
+}
